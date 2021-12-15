5f9da3a984b6e97d26998ae4a3e08c19942b5ca5
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2021-12-15 16:21:45 +0000

    menu de parâmetros e de reservas

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
 class Main {
 public static void main(String[] args) {
 
-  menuPrincipal();  
+  menuPrincipal();
+  parametroSistema();
+  reservaPassagem();
 
 }
 public static void menuPrincipal() {
   System.out.println("Menu Principal");
-  }
+}
+public static void parametroSistema() {
+  System.out.println("Parâmetros do Sistema");
+}
+public static void reservaPassagem() {
+  System.out.println("Reserva de passagens");
+}
+
 }
