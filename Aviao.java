bd4e1a98399cf84bc9c1178398db934fb80ba88e
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2021-12-18 21:36:44 +0000

    Aviao do Mauricio

--- Aviao.java
+++ Aviao.java
@@ -1,27 +1,26 @@
 public class Aviao extends Aeronave {
-  
-  //atributos
-  public Passageiro[][] lugares;
-  int fileiras, assentos;
-
-  //método construtor
- Aviao(String modelo, int fileiras, int assentos)
- {
-   super(modelo);
-   Passageiro lugares[][] = new Passageiro[fileiras][assentos];
- }
-
-  //demais métodos
-  public Passageiro[][] getPassageiro(int fileira, int acentos)
-  {
-    return lugares;
-  }
-  public boolean verificaLugarOcupado(int fileira, int acento)
-  {
-    return true;
-  }
-  public void setPassageiro(int fileira, int acento, Passageiro lugares)
-  {
-    
-  }
+	public Passageiro[][] lugares;
+	
+	public Aviao (String modelo, int fileiras, int assentos) {
+		super(modelo);
+		lugares = new Passageiro[fileiras][assentos];
+	}
+	
+	public void setPassageiro (int fileira, int assento, Passageiro passageiro) {
+		lugares[fileira][assento] = new Passageiro("Maria", "123");
+	}
+	
+	public Passageiro getPassageiro(int fileira, int assento) {
+		if (lugares[fileira][assento] != null)
+			return lugares[fileira][assento];
+		else
+		return null;	
+	}
+	
+	public boolean verificaLugarOcupado (int fileira, int assento) {
+		if (lugares[fileira][assento] != null)
+			return true;
+		else
+			return false;
+	}
 }
