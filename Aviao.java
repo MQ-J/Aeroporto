fef2bb153efb16cd7401f9a379174fc67451ebbf
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2021-12-18 21:52:41 +0000

    comentários no Aviao

--- Aviao.java
+++ Aviao.java
@@ -1,15 +1,20 @@
 public class Aviao extends Aeronave {
+
+  //atributos
 	public Passageiro[][] lugares;
 	
+  //método construtor
 	public Aviao (String modelo, int fileiras, int assentos) {
 		super(modelo);
 		lugares = new Passageiro[fileiras][assentos];
 	}
 	
+  //método set
 	public void setPassageiro (int fileira, int assento, Passageiro passageiro) {
 		lugares[fileira][assento] = passageiro;
 	}
 	
+  //método get
 	public Passageiro getPassageiro(int fileira, int assento) {
 		if (lugares[fileira][assento] != null)
 			return lugares[fileira][assento];
@@ -17,6 +22,7 @@ public class Aviao extends Aeronave {
 		return null;	
 	}
 	
+  //método de verificar lugar disponível
 	public boolean verificaLugarOcupado (int fileira, int assento) {
 		if (lugares[fileira][assento] != null)
 			return true;
