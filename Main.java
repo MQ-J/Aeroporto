2fdc096ad9cdb95c84555cca90d62bcd11ac4f76
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2021-12-19 00:04:22 +0000

    cadastraAviao

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public static void parametroSistema() {
   int menu = Integer.parseInt(JOptionPane.showInputDialog("Parâmetros do Sistema\n\t1 - Cadastrar Aeronave\n\t2 - Cadastrar Voo\n\t3 - Voltar"));
 
   switch(menu){
-    case 1: System.out.println("cadastra aeronave"); break;
+    case 1: cadastraAviao(); break;
     case 2: System.out.println("cadastra voo"); break;
     case 3: menuPrincipal(); break;
     default: JOptionPane.showMessageDialog(null, "Opção inválida"); parametroSistema();
@@ -37,6 +37,20 @@ public static void reservaPassagem() {
     case 4: menuPrincipal(); break;
     default: JOptionPane.showMessageDialog(null, "Opção inválida"); reservaPassagem();
   }
+}
+//CADASTRO DE AVIÃO
+public static void cadastraAviao() {
+
+  //usuário digita dados do avião
+  String modelo = JOptionPane.showInputDialog("Digite o modelo do avião");
+  int fileiras = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de fileiras:"));
+  int assentos = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de assentos por fileira:"));
+
+  //novo avião é criado
+  new Aviao(modelo, fileiras, assentos);
+
+  //aviso de conclusão para o usuário
+  JOptionPane.showMessageDialog(null, "Avião" + modelo + " criado com sucesso.");
 }
 
 }
